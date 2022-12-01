1b04d4e33f2bd6ff234a199513880acbdbe420ca
Andrey <teryaev.2004@mail.ru>
2022-12-01 16:30:39 +0300

    add-firebase

--- src/components/pages/Auth/Auth.tsx
+++ src/components/pages/Auth/Auth.tsx
@@ -1,10 +1,90 @@
-import React, {FC} from 'react';
+import { Alert, Button, ButtonGroup, Grid, TextField } from "@mui/material";
+import React, { FC } from "react";
+import { IUserData } from "./types";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const Auth: FC = () => {
+  const [isRegForm, setIsRegForm] = React.useState<boolean>(false);
+  const [userData, setUserData] = React.useState<IUserData>({
+    email: "",
+    password: "",
+  } as IUserData);
+  const [error, setError] = React.useState<string>("");
+
+  const handleLogin = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const auth = getAuth();
+
+    if (isRegForm) {
+      try {
+        await createUserWithEmailAndPassword(
+          auth,
+          userData.email,
+          userData.password
+        );
+      } catch (err: any) {
+        err.message && setError(err.message);
+      }
+    } else {
+      try {
+        await signInWithEmailAndPassword(
+          auth,
+          userData.email,
+          userData.password
+        )
+      } catch(err: any) {
+        err.message && setError(err.message);
+      }
+    }
+
+    setUserData({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
-    <div>
-      Auth
-    </div>
+    <>
+      {error && <Alert severity="error" style={{marginBottom: 20}}>{error}</Alert>}
+      <Grid display="flex" justifyContent="center" alignItems="center">
+        <form onSubmit={handleLogin}>
+          <TextField
+            type="email"
+            label="Email"
+            variant="outlined"
+            value={userData.email}
+            onChange={(e) =>
+              setUserData({ ...userData, email: e.target.value })
+            }
+            sx={{ display: "block", marginBottom: 3 }}
+            required
+          />
+          <TextField
+            type="password"
+            label="Password"
+            variant="outlined"
+            value={userData.password}
+            onChange={(e) =>
+              setUserData({ ...userData, password: e.target.value })
+            }
+            sx={{ display: "block" }}
+            required
+          />
+          <ButtonGroup
+            variant="outlined"
+            aria-label="outlined button group"
+            sx={{ marginTop: "20px" }}
+          >
+            <Button type="submit" onClick={() => setIsRegForm(false)}>
+              Auth
+            </Button>
+            <Button type="submit" onClick={() => setIsRegForm(true)}>
+              Register
+            </Button>
+          </ButtonGroup>
+        </form>
+      </Grid>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,16 @@ import "./index.css";
 import Layout from "./components/layout/Layout";
 import Routes from "./components/Routes/Routes";
 import { BrowserRouter } from "react-router-dom";
+import * as firebase from "firebase/app";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCO4aI_Qvv2YaMfxwFUzgdbh8bXuLO3peQ",
+  authDomain: "vk-copy-57e5b.firebaseapp.com",
+  projectId: "vk-copy-57e5b",
+  storageBucket: "vk-copy-57e5b.appspot.com",
+  messagingSenderId: "198112997326",
+  appId: "1:198112997326:web:0af426aebb9069a1e2859f" 
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
